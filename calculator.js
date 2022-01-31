b475db1dab6d2c1b9d02ab21ff50e7211bd65846
gary <gary.white@lodgit.com>
2022-01-31 17:49:31 +0100

    finished lesson

--- calculator.js
+++ calculator.js
@@ -1,14 +1,28 @@
 //jshint esversion:6
 
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 //define route
-app.get("/", function(request, response){
-    response.sendFile(__dirname + "/index.html");
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
     // console.log(__dirname);
 });
+
+app.post("/", function(req, res){
+
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+
+    var result = num1 + num2;
+
+    res.send("The result of the calculation is " + result);
+});
+
+// console.log(req.body.num1);
 
 //spin up server
 app.listen(3000, function(){
