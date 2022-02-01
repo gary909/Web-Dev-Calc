e099fa24641af4a6eeda6083a672bc7d97670af5
gary <gary.white@lodgit.com>
2022-02-01 17:19:33 +0100

    finished files

--- calculator.js
+++ calculator.js
@@ -29,10 +29,10 @@ app.get("/bmicalculator", function(req, res){
 
 app.post("/bmicalculator", function(req, res){
 
-    var weight = Number(req.body.weight);
-    var height = Number(req.body.height);
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
 
-    var bmiRes = weight + height;
+    var bmiRes = weight / (height * height);
 
     res.send("The BMI is " + bmiRes);
 });
